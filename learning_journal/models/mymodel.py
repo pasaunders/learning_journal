1803741a462708c849ba799e36d58f3072c8b68c
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-22 13:47:08 -0800

    removed date

--- learning_journal/models/mymodel.py
+++ learning_journal/models/mymodel.py
@@ -14,7 +14,7 @@ class MyModel(Base):
     id = Column(Integer, primary_key=True)
     title = Column(Unicode)
     body = Column(Unicode)
-    creation_date = Column(Date)
+    creation_date = Column(Unicode)
 
 
 Index('my_index', MyModel.title, unique=True, mysql_length=255)

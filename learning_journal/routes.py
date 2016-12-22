563bc52a000f826b5de9ec4405cc7b7eff25bdbc
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-22 14:00:02 -0800

    added docstrings

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -1,6 +1,10 @@
+"""Route for learning journal app."""
+
+
 def includeme(config):
+    """Add routes to include in our pyramid app."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    # config.add_route('new', '/journal/new-entry')
+    config.add_route('new', '/journal/new-entry')
     config.add_route('detail', '/journal/{id:\d+}')
-    # config.add_route('edit', '/journal/{id:\d+}/edit-entry')
+    config.add_route('edit', '/journal/{id:\d+}/edit-entry')
